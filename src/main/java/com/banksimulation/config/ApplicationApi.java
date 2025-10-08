6ca126ab4887da8222af8651d459912213265e24
harish-sai-nallam <harishsainallam@gmail.com>
2025-10-08 12:02:43 +0530

    Initialization of Jax-RS Application with base path /bank

---
+++ src/main/java/com/banksimulation/config/ApplicationApi.java
@@ -0,0 +1,10 @@
+package com.banksimulation.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/bank")
+public class ApplicationApi extends Application {
+
+}
+

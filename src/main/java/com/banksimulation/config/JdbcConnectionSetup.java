144623b074f202900f96b1a5fbcb5852d09ee145
harish-sai-nallam <harishsainallam@gmail.com>
2025-10-08 16:19:35 +0530

    Jdbc Connection without any hard-coded values

--- src/main/java/com/banksimulation/config/JdbcConnectionSetup.java
+++ src/main/java/com/banksimulation/config/JdbcConnectionSetup.java
@@ -1,17 +1,30 @@
 package com.banksimulation.config;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class JdbcConnectionSetup {
-    private static final String username="root";
-    private static final String password="localhost@123";
-    private static final String url="jdbc:mysql://localhost:3306/bank_simulation";
-    private static final String dbName="bank_simulation";
+    private static final String username;
+    private static final String password;
+    private static final String url;
      static Connection connection;
-
-
+     static {
+         Properties properties = new Properties();
+         try (InputStream input = JdbcConnectionSetup.class.getClassLoader().getResourceAsStream("application.properties")) {
+             if (input != null) {
+                 properties.load(input);
+             }
+         }
+         catch (IOException e){e.printStackTrace();}
+         url = properties.getProperty("url");
+         username = properties.getProperty("username");
+         password = properties.getProperty("password");
+     }
 
     public static Connection getConnection () throws SQLException,ClassNotFoundException{
         if(connection==null || connection.isClosed()){
